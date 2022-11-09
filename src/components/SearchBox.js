609fed3da84cd6e64ef2f4b1294c5846cbf99acb
nandalalainfotech <nandalalainfotech@gmail.com>
2022-11-09 15:56:10 +0530

    responsive navbar changes done by Naresh

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -12,7 +12,7 @@ export default function SearchBox(props) {
     <form className="search" onSubmit={submitHandler} >
       <div className="row">
         <div className="form-group has-search">
-          <span className="fa fa-camera form-control-feedback"></span>
+          <span className="fa fa-search form-control-feedback"></span>
           <input type="text" className="form-control" placeholder="Search for Products,brands and more" onChange={(e)=>setName(e.target.value)}></input>
           <button type="submit" className="search-submit"><i className="fa fa-search">Search</i></button>
         </div>
